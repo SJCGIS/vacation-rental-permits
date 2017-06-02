7758d8c773e293743325ac2a3b745f6b9a3af6ff
Nick Peihl <nickp@sanjuanco.com>
2017-06-02 12:33:36 -0700

    Use FeatureLayer instead of Map Service to better UI

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /* global L */
 require('leaflet')
 var Esri = require('esri-leaflet')
+require('esri-leaflet-renderers')
 require('leaflet-sidebar-v2')
 var bel = require('bel')
-var series = require('run-series')
 var octicons = require('octicons')
 
 var map = L.map('map', {
@@ -21,102 +21,113 @@ var basemap = Esri.tiledMapLayer({
   url: 'https://sjcgis.org/arcgis/rest/services/Basemaps/General_Basemap_WM/MapServer'
 }).addTo(map)
 
-var vacationRentals = Esri.dynamicMapLayer({
-  url: 'https://sjcgis.org/arcgis/rest/services/LandUse/Vacation_Rental_Permits/MapServer',
-  layers: [0]
+var vacationRentals = Esri.featureLayer({
+  url: 'https://sjcgis.org/arcgis/rest/services/LandUse/Vacation_Rental_Permits/MapServer/0',
+  fields: ['OBJECTID', 'Permit_No', 'Customer_LName', 'Permit_Status', 'Decision_Date']
 }).addTo(map)
-
-vacationRentals.bindPopup(function (err, fc) {
-  if (err || fc.features.length === 0) {
-    return false
-  } else {
-    var props = fc.features[0].properties
-    return bel`<ul class="list pl0">${Object.keys(props)
-      .filter(function (key) {
-        return ['Permit Number', 'Last Name', 'Status', 'Decision Date'].indexOf(key) > -1
-      })
-      .map(function (key) {
-        return bel`<li class="flex items-center lh-copy pa3 ph0-l bb b--black-10">
-      <div class="db f6 black-80">
-      <b>${key}:</b>
-      <span>${props[key]}</span>
-      </li>`
-      })
-    }</ul>`
-  }
-})
 
 var sidebar = L.control.sidebar('sidebar').addTo(map)
 
-// Create panes in order of display. e.g. home, legend, etc
-series([
-  function (cb) {
-    vacationRentals.metadata(function (err, data) {
-      if (err) cb(err)
-      else {
-        var el = bel`<div class="sidebar-pane">
-     <h1 class="sidebar-header f5">${data.documentInfo.Title}
-     ${sidebarClose()}</h1>
-     ${data.serviceDescription.split(/\r?\n/g).map(function (line) {
-       return bel`<p class="f5 measure lh-copy">${line}</p>`
-     })}
-   </div>`
-
-        var panelContent = {
-          id: 'home',
-          tab: octicons['home'].toSVG({
-            'aria-label': 'Toggle Home pane',
-            'width': '20',
-            'height': '20',
-            'fill': 'currentcolor',
-            'viewBox': '0 0 20 20',
-            'class': 'v-mid'
-          }),
-          pane: el
-        }
-        sidebar.addPanel(panelContent)
-        sidebar.open('home')
-        cb(null)
-      }
-    })
-  },
-  function (cb) {
-    createLegend(vacationRentals.service.options.url, function (err, el) {
-      if (err) cb(err)
-      else {
-        sidebar.addPanel({
-          id: 'legend',
-          tab: octicons['list-unordered'].toSVG({
-            'aria-label': 'Toggle Legend pane',
-            'width': '20',
-            'height': '20',
-            'fill': 'currentcolor',
-            'viewBox': '0 0 20 20',
-            'class': 'v-mid'
-          }),
-          pane: el
-        })
-        cb(null)
-      }
-    })
-  }
-], function (err) {
+//Create panes in order of display. e.g. home, legend, etc
+vacationRentals.metadata(function (err, data) {
   if (err) console.error(err)
+  else {
+    createHomePanel(data)
+    createLegendPanel(data)
+    createPopups(data)
+  }
 })
 
-function createLegend (serviceUrl, cb) {
-  var legendUrl = serviceUrl + '/legend'
-  Esri.get(legendUrl, {
-    f: 'json'
-  }, function (err, data) {
-    if (err) cb(err)
-    var el = bel`<div class="sidebar-pane">
-      <h1 class="sidebar-header f5">Legend
+function createHomePanel (data) {
+  var el = bel`<div class="sidebar-pane">
+    <h1 class="sidebar-header f5">Vacation Rental Permits
       ${sidebarClose()}</h1>
-      ${data.layers.map(createLayerLegend)}
-    </div>`
-    cb(null, el)
+    ${data.description.split(/\r?\n/g).map(function (line) {
+      return bel`<p class="f5 measure lh-copy">${line}</p>`
+    })}
+  </div>`
+  sidebar.addPanel({
+    id: 'home',
+    tab: octicons['home'].toSVG({
+      'aria-label': 'Toggle Home pane',
+      'width': '20',
+      'height': '20',
+      'fill': 'currentcolor',
+      'viewBox': '0 0 20 20',
+      'class': 'v-mid'
+    }),
+    pane: el
   })
+  sidebar.open('home')
+}
+
+function createLegendPanel (data) {
+  var el = createLegend(data)
+  sidebar.addPanel({
+    id: 'legend',
+    tab: octicons['list-unordered'].toSVG({
+      'aria-label': 'Toggle Legend pane',
+      'width': '20',
+      'height': '20',
+      'fill': 'currentcolor',
+      'viewBox': '0 0 20 20',
+      'class': 'v-mid'
+    }),
+    pane: el
+  })
+}
+
+function createPopups (data) {
+  var fieldInfo = data.fields.reduce(function (acc, cur, i) {
+    acc[cur['name']] = cur
+    return acc
+  }, {})
+  vacationRentals.bindPopup(function (layer) {
+    var props = layer.feature.properties
+    var el = bel`<ul class="list pl0">${Object.keys(props)
+    .map(function (key) {
+      return bel`<li class="flex items-center lh-copy measure pa1 ph0-l bb b--black-10">
+      <div class="db f7 f6-ns black-80">
+      <b>${fieldInfo[key]['alias']}:</b>
+      <span class="pl1">${format(key, props[key])}</span>
+      </div>
+      </li>`
+    })
+  }</ul>`
+    return el
+  })
+
+  function format (k, v) {
+    switch (fieldInfo[k]['type']) {
+      case 'esriFieldTypeSmallInteger':
+      case 'esriFieldTypeInteger':
+      case 'esriFieldTypeSingle':
+      case 'esriFieldTypeDouble':
+      case 'esriFieldTypeString':
+      case 'esriFieldTypeOID':
+      case 'esriFieldTypeGUID':
+      case 'esriFieldTypeGlobalID':
+      case 'esriFieldTypeXML':
+        return v
+      case 'esriFieldTypeDate':
+        return new Date(v).toLocaleDateString()
+      case 'esriFieldTypeGeometry':
+      case 'esriFieldTypeBlob':
+      case 'esriFieldTypeRaster':
+        return null
+    }
+  }
+}
+
+
+
+function createLegend (data) {
+  var el = bel`<div class="sidebar-pane">
+    <h1 class="sidebar-header f5">Legend
+    ${sidebarClose()}</h1>
+    ${createLayerLegend(data)}
+  </div>`
+  return el
 }
 
 function sidebarClose () {
@@ -134,14 +145,16 @@ function sidebarClose () {
 
 function createLayerLegend (layer) {
   return bel`<div>
-    <h2 class="f5 lh-copy">${layer.layerName}</h2>
-    <ul class="list p10 mt0 measure center">${layer.legend.map(createLegendItem)}</ul>
+    <h2 class="f5 lh-copy">${layer.name}</h2>
+    <ul class="list p10 mt0 measure center">
+      ${layer.drawingInfo.renderer.uniqueValueInfos.map(createLegendItem)}
+    </ul>
   </div>`
 }
 
 function createLegendItem (item) {
   return bel`<li class="flex items-center lh-copy pa3 ph0-l bb b--black-10">
-    <img class="pr2" src="data:${item.contentType};base64,${item.imageData}" />
+    <img class="pr2" src="data:${item.symbol.contentType};base64,${item.symbol.imageData}" />
     <span class="f6 db black-80">${item.label}</span>
   </li>`
 }
